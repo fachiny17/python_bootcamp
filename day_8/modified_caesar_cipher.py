439780896d92feec76d53af8a2fdc8643263f87b
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-07-28 05:07:48 +0100

    added my python bootcamp activities

--- day_8/modified_caesar_cipher.py
+++
@@ -1,8 +0,0 @@
-from alphabets_and_logo import alphabet
-
-def caesar(start_text, shift_amount, cipher_direction):
-    end_text =""
-    if cipher_direction == "decode":
-        shift_amount *= -1
-    for char in start_text:
-        
