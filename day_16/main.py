f1f5cba1dfe1d1de29125dadbe173fbe87464a74
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-09-28 00:26:14 +0100

    made an update

--- day_16/main.py
+++ day_16/main.py
@@ -1,3 +1,8 @@
-from turtle import Turtle
-timy = Turtle()
-print(timy)
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikatchi","Mkpamkpab","Lopikop"])
+table.add_column("Type",["Electric","Marrr","Ciopoi"])
+
+table.align = "l" #left alignment
+
+print(table)
