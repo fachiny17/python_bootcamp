e8f08d2d76ea2188cf89896e1b50e8f06764d629
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-09-29 23:54:09 +0100

    made an update

--- day_16/oop_coffee_machine/main.py
+++ day_16/oop_coffee_machine/main.py
@@ -19,4 +19,4 @@ while is_on:
     else:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            coffee_maker.make_coffee(drink) erer
+            coffee_maker.make_coffee(drink)
