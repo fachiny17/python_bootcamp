316669036fea9a2bc4a4953c8f09a1b2f94eac3e
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-09-30 00:33:11 +0100

    mae an update

--- my_oop_projects/library_management_system/book.py
+++ my_oop_projects/library_management_system/book.py
@@ -2,3 +2,6 @@ class Book(self, author, title):
     def __init__(self, title, author):
         self.title = title
         self.author = author
+        
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
---
+++ my_oop_projects/library_management_system/library.py
@@ -0,0 +1,15 @@
+class Library:
+    def __init__(self):
+        self.books = []
+        
+    def add_book(self, book):
+        self.books.append(book)
+        print(f"The book {book.title} has been added to library.")
+    
+    def remove_book(self, title):
+        for book in self.books:
+            if self.title == title:
+                self.books.remove(book)
+                print(f"The book '{title}' has been removed from library.")
+                return
+        print(f"The book '{title}' not fond in the library.")
