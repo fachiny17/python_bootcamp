c038d0893cb9e5dd62081aa655b53ff981aac7aa
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-09-30 21:35:43 +0100

    made an update

---
+++ my_oop_projects/student_management_system/main.py
@@ -0,0 +1,8 @@
+from student import Student
+from student_manager import StudentManager
+
+student_manager = StudentManager()
+
+while True:
+    print("\nWelcome to our Student Management Menu")
+    print()
--- my_oop_projects/student_management_system/student_manager.py
+++ my_oop_projects/student_management_system/student_manager.py
@@ -6,10 +6,18 @@ class StudentManager:
         self.students.append(student)
         print(f"Student {student.name} has been added.")
         
-    def remove_students(self, student_id):
+    def remove_student(self, student_id):
         for student in self.students:
             if self.student_id == student_id:
                 self.students.remove(student)
                 print(f"Student {student.name} has been removed.")
                 return
-            print("Student not found.")
+        print("Student not found.")
+        
+    def display_students(self, student_id):
+        if student_id not in self.studnts:
+            print("No student available.")
+        else:
+            print("Students List:")
+            for student in self.students:
+                print(student)
