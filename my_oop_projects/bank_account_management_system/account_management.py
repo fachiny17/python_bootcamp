be35d091b9aaa01eedc825d71f364207a9fc9857
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-02 16:59:16 +0100

    made an update

--- my_oop_projects/bank_account_management_system/account_management.py
+++ my_oop_projects/bank_account_management_system/account_management.py
@@ -10,9 +10,26 @@ class BankAccount:
         return f"Owner: {self.owner}, Account number: {self.account_number}, Account Balance: {self.balance}"
     
     def deposit(self, amount):
+        amount = input("Enter the amount you want to deposit: ")
         self.balance += amount
         print(f"Deposited ${amount}. New balance is ${self.balance}")
         
     def withdraw(self, amount):
+        amount = input("Entr the amount you want to withdraw: ")
         if amount <= self.balance:
             self.balance -= amount
+            print(f"Debit alert! \nDear {self.owner}, you have successfully withdrawn ${amount}. New balance is ${self.balance}. \nThanks for banking with us.")
+        else:
+            print(f"Insufficient balance! \nYou cannot withdraw ${amount} because your balance is only {self.balance}.")
+        
+    def check_balance(self):
+        print(f"Account balance: ${self.balance}")
+        
+
+account = BankAccount()
+while True:
+    print("\nBank Account Menu")
+    print("1. Deposit")
+    print("2. Withdraw")
+    print("3. Check Balanace")    
+    
