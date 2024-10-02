3836f6ad572a47ca7e403145b859a808ec71355d
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-02 16:36:44 +0100

    made an update

--- my_oop_projects/bank_account_management_system/account_management.py
+++ my_oop_projects/bank_account_management_system/account_management.py
@@ -1,7 +1,18 @@
 class BankAccount:
-    def __init__(self, account_number, balance):
+    def __init__(self, account_number, owner, balance):
         self.account_number = account_number
+        self.owner = owner
         self.balance = balance
         
+    balance = 0
+        
     def __str__(self):
-        return f"Account number: {self.account_number}, Account Balance: {self.balance}"
+        return f"Owner: {self.owner}, Account number: {self.account_number}, Account Balance: {self.balance}"
+    
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited ${amount}. New balance is ${self.balance}")
+        
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
