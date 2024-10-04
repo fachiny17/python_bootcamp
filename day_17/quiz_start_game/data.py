07fc8e128736d183f961872634b74f179e84667c
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-04 09:08:37 +0100

    made an update

---
+++ day_17/quiz_start_game/data.py
@@ -0,0 +1,6 @@
+question_data = [
+    {"text": "The colour of snow is white.", "answer": "True"},
+    {"text": "The capital of Austria is Vienna.", "answer": "True"},
+    {"text": "There are fifty-nine(59) countries in Africa.", "answer": "False"},
+    {"text": "Mount Fuji is located in Kenya.", "answer": "False"},
+]
