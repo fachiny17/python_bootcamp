4dfb0267b177fb7a88683c5db0e7a8d08b39b274
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-06 17:11:52 +0100

    made an update

--- day_18/multiple_shapes.py
+++ day_18/multiple_shapes.py
@@ -1,9 +1,19 @@
+# TODO:
+# Draw a triangle, square, pentagon, hexagon, heptagon, octagon, nonagon and decagon.
+# Each of the shapes should have a random colour and each side should be 100 in terms of lenght
+
 from turtle import Turtle, Screen
 
 multi = Turtle()
 
 multi.shape("turtle")
 multi.color("brown")
+
+for triangle in range(3):
+    multi.right(120)
+    multi.forward(100)
+
+for
 
 screen = Screen()
 screen.exitonclick()
