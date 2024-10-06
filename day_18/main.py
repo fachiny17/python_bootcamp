3a2f7cf82a0d9869c1dbb77e0c13e8cbefc59de5
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-06 15:44:30 +0100

    made an update

--- day_18/main.py
+++ day_18/main.py
@@ -3,5 +3,11 @@ from turtle import Turtle, Screen
 timmy_turtle = Turtle()
 
 
+timmy_turtle.shape("turtle")
+timmy_turtle.color("brown")
+timmy_turtle.forward(100)
+timmy_turtle.right(90)
+
+
 screen = Screen()
 screen.exitonclick()
