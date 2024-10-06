70b8714cfd68f21aaa875c821993583031172be5
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-06 16:48:22 +0100

    made an update

--- day_18/dashed_line.py
+++ day_18/dashed_line.py
@@ -2,6 +2,14 @@ from turtle import Turtle, Screen
 
 dashed_turtle = Turtle()
 
+dashed_turtle.shape("turtle")
+dashed_turtle.color("brown")6
+
+for move in range(15):
+    dashed_turtle.forward(10)
+    dashed_turtle.penup()
+    dashed_turtle.forward(10)
+    dashed_turtle.pendown()
 
 screen = Screen()
 screen.exitonclick()
