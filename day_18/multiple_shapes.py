b81e041838a6315ac188221679a609407a740e5f
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-06 17:35:22 +0100

    made an update

--- day_18/multiple_shapes.py
+++ day_18/multiple_shapes.py
@@ -52,3 +52,23 @@ screen = Screen()
 screen.exitonclick()
 
 # Excellently done. Congrats! for doing this yourself
+
+# ------------------------------------------------------------------------------------------------------------------
+# Professional approach
+
+# import turtle as t
+# import random
+#
+# tim = t.Turtle()
+#
+# colours = ["red","brown","black","orange","green","indigo"]
+#
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for move in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+#
+# for shape_side_n in range(3, 11):
+#     tim.color(random.choice(colours))
+#     draw_shape(shape_side_n)
