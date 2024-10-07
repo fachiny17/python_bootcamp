9d128e88432593eb304514912460d265d8049532
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-07 17:38:42 +0100

    made an update

--- day_18/multiple_shapes.py
+++ day_18/multiple_shapes.py
@@ -66,8 +66,8 @@ screen.exitonclick()
 # def draw_shape(num_sides):
 #     angle = 360 / num_sides
 #     for move in range(num_sides):
-#         tim.forward(100)
-#         tim.right(angle)
+#         tim.forward(100) 
+#         tim.right(angle) 
 #
 # for shape_side_n in range(3, 11):
 #     tim.color(random.choice(colours))
