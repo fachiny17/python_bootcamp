ce0c8ed391e47e3f954d3da53b7cd15c86eb6327
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-09 17:03:19 +0100

    made an update

--- day_18/random_walk.py
+++ day_18/random_walk.py
@@ -1,10 +1,28 @@
-from turtle import Turtle, Screen
+# from turtle import Screen
+import turtle as t
+import random
 
-walk = Turtle()
-walk.shape("turtle")
+tim = t.Turtle()
+tim.shape("turtle")
+t.colormode(255)
 
-colours = ["red", "brown", "black", "orange", "green", "indigo"]
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
 
 
-screen = Screen()
+directions = [0, 90, 180, 270]
+tim.pensize(15)  # pensize is same as width
+tim.speed("fastest")
+
+for move in range(300):
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+    tim.color(random_color())
+
+screen = t.Screen()
 screen.exitonclick()
