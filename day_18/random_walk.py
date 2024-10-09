b1bb55a06e951e58c7e166aed81c42e22ef8721c
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-09 11:31:45 +0100

    made an update

---
+++ day_18/random_walk.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+walk = Turtle()
+walk.shape("turtle")
+
+colours = ["red", "brown", "black", "orange", "green", "indigo"]
+
+
+screen = Screen()
+screen.exitonclick()
