54e4978f8ee3f5532fb8ee8cd5d886a9a787fbf5
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-14 06:58:50 +0100

    made an update

---
+++ day_19/prac.py
@@ -0,0 +1,36 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def turn_left():
+    tim.left(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(move_forward, "f")
+screen.onkey(move_backward, "b")
+screen.onkey(turn_left, "l")
+screen.onkey(turn_right, "r")
+screen.onkey(clear, "c")
+screen.exitonclick()
