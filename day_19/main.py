ec0adfecd0891e3a48500a35166f68227d3a9e8d
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-14 04:44:56 +0100

    made an update

--- day_19/main.py
+++ day_19/main.py
@@ -4,10 +4,17 @@ tim = Turtle()
 screen = Screen()
 
 
-def move_forward():
-    tim.forward(10)
+# def move_forward():
+#    tim.forward(10)
+
+def movement(move_forward, move_backward):
+    move_forward = tim.forward(10)
+    move_backward = tim.backward(10)
 
 
-screen.listen()
-screen.onkey(key="space", fun=move_forward)
+movement(move_forward=10, move_backward=10)
+
+screen.listen()()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="w", fun=move_backward)
 screen.exitonclick()
