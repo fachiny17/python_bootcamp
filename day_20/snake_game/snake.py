b36441227577186a09c5fa4096e62b80b5d20b04
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-17 16:45:06 +0100

    made an update

--- day_20/snake_game/snake.py
+++ day_20/snake_game/snake.py
@@ -22,4 +22,4 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-            self.segments[0].forward(MOVE_DISTANCE)
+        self.segments[0].forward(MOVE_DISTANCE)
