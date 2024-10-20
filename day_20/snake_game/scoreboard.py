399de377e597d4f749d9caa4af0af5346252bf2e
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-20 10:21:04 +0100

    made an update

--- day_20/snake_game/scoreboard.py
+++ day_20/snake_game/scoreboard.py
@@ -3,5 +3,10 @@ from turtle import Turtle
 
 class Scoreboard(Turtle):
     def __init__(self):
-        super()__init__()
-        self.
+        super().__init__()
+        self.score = []
+
+    def score(self):
+
+        for num in self.score:
+            if
