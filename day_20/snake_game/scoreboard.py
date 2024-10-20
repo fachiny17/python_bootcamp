fdc5e8b18795defac0282555bc0f3cf4e1affa02
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-20 10:27:37 +0100

    made an update

--- day_20/snake_game/scoreboard.py
+++ day_20/snake_game/scoreboard.py
@@ -5,10 +5,13 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
         self.write(f"Score: {self.score}", align="center",
                    font=("Arial", 24, "normal"))
 
     def score(self):
 
         for num in self.score:
-            if
+            pass
