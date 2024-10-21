e89ae25dc7d968b22d26eccc5aa74a8c384b23d3
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-10-21 13:44:35 +0100

    made an update

--- day_20/snake_game/main.py
+++ day_20/snake_game/main.py
@@ -29,28 +29,30 @@ if user_choice:
 
 while game_is_on:
     if user_choice not in options:
-
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
-
-    # Detect collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
-
-    # Detect collision with wall
-    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        print(f"Invalid option, please input either {', '.join(options)}.")
         game_is_on = False
-        scoreboard.game_over()
+    else:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
 
-    # Detect collision with tail
-    for segment in snake.segments[1:]:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+        # Detect collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
+
+        # Detect collision with wall
+        if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
             game_is_on = False
             scoreboard.game_over()
+
+        # Detect collision with tail
+        for segment in snake.segments[1:]:
+            if segment == snake.head:
+                pass
+            elif snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
 
 screen.exitonclick()
