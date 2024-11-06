1da082b4b064f9d98a9970a3e90b6bd60792420d
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-11-06 02:05:50 +0100

    made an update

--- day_20/snake_game/main.py
+++ day_20/snake_game/main.py
@@ -4,68 +4,82 @@ import time
 from food import Food
 from scoreboard import Scoreboard
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
-screen.tracer(0)
 
-options = ["yes", "no"]
-user_choice = None
+def play_game():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("My Snake Game")
+    screen.tracer(0)
 
-# Function to get user choice
+    options = ["yes", "no"]
+    user_choice = None
 
+    # Function to get user choice
 
-def get_user_choice():
-    return screen.textinput(title="Make your choice", prompt="Do you want to start the game? (yes/no): ").lower()
+    def get_user_choice():
+        return screen.textinput(title="Make your choice", prompt="Do you want to start the game? (yes/no): ").lower()
 
+    # Main logic to handle user input
+    while user_choice not in options:
+        user_choice = get_user_choice()
 
-# Main logic to handle user input
-while user_choice not in options:
-    user_choice = get_user_choice()
+        if user_choice not in options:
+            print(f"Invalid option, please input either {', '.join(options)}.")
 
-    if user_choice not in options:
-        print(f"Invalid option, please input either {', '.join(options)}.")
+        if user_choice == "no":
+            print("Quitting Game!")
+            screen.bye()
 
-    if user_choice == "no":
-        print("Quitting Game!")
-        screen.bye()
+    # If the user chose "yes", initialize game elements
+    snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
 
-# If the user chose "yes", initialize game elements
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
 
+        # Detect collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
-
-    # Detect collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
-
-    # Detect collision with wall
-    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        game_is_on = False
-        scoreboard.game_over()
-
-    # Detect collision with tail
-    for segment in snake.segments[1:]:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+        # Detect collision with wall
+        if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
             game_is_on = False
             scoreboard.game_over()
+            play_again(screen)
 
-screen.exitonclick()
+        # Detect collision with tail
+        for segment in snake.segments[1:]:
+            if segment == snake.head:
+                pass
+            elif snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
+                play_again(screen)
+
+    screen.exitonclick()
+
+
+def play_again(screen):
+    again_choice = screen.textinput(
+        title="Play Again", prompt="Do you want to play again? (yes/no)").lower()
+    if again_choice == "yes":
+        play_game()
+    else:
+        print("Thank you for playing!")
+        screen.bye()
+
+
+play_game()
