5e53a9848a1d469e8ac31dbb0a8a3b73162b11fe
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-11-09 10:58:49 +0100

    made an update

--- day_22/pong.py
+++ day_22/pong.py
@@ -15,8 +15,8 @@ ball = Ball()
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
+screen.onkey(l_paddle.go_up, "u")
+screen.onkey(l_paddle.go_down, "d")
 
 game_is_on = True
 while game_is_on:
