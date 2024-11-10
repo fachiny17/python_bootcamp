bab537b3fb4a28ca3a4ac61b53819a398b218201
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-11-10 12:38:49 +0100

    made an update

--- day_23/main.py
+++ day_23/main.py
@@ -8,6 +8,10 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+screen.listen()
+screen.onkey(Up, "Up")
+screen.onkey(Down, "Down")
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
--- day_23/player.py
+++ day_23/player.py
@@ -1,7 +1,12 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
 class Player:
-    pass
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
