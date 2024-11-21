da2afa869abfbf058c820757a483c5911261ced7
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-11-21 13:36:36 +0100

    made an update

--- my_oop_projects/library_management_system/book.py
+++
@@ -1,7 +0,0 @@
-class Book:
-    def __init__(self, author, title):
-        self.author = author
-        self.title = title
-        
-    def __str__(self):
-        return f"'{self.title}' by {self.author}"
