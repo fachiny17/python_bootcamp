b95dc0458e63dafeb6f560eddda43a597a1b6f57
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-12-19 20:37:00 +0100

    made a update

---
+++ day_26/nato_alphabet_project/main.py
@@ -0,0 +1,26 @@
+import pandas
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+# Looping through dictionaries:
+for (key, value) in student_dict.items():
+    # Access key and value
+    pass
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+print(data.to_dict())
+# Loop through rows of a data frame
+for (index, row) in student_data_giframe.iterrows():
+    # Access index and row
+    # Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
