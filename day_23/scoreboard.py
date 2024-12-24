144aeeb44084e75ce89012c0a33386bbf8d2e3b1
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-12-24 01:37:15 +0100

    made an update

--- day_23/scoreboard.py
+++ day_23/scoreboard.py
@@ -20,6 +20,14 @@ class Scoreboard(Turtle):
         self.write(f"Score: {self.score} High Score: {
                    self.high_score}", align=ALIGNMENT, font="FONT")
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
+
     def game_over(self):
         self.goto(0, 0)
         self.write(f"GAME OVER.\n Score: {
