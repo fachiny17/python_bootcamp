ad59e244d5d63e0ae1e5ea9e59d26c679620bcb8
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2025-01-04 21:02:29 +0100

    made an update

--- day_27/prac.py
+++ day_27/prac.py
@@ -20,5 +20,10 @@ calculate(add=3, multiply=5)
 class Car:
 
     def __init__(self, **kw):
-        self.make = kw["make"]
-        self.model = kw["model"]
+        self.make = kw.get("make")  # same as self.make = kw["make"]
+        self.model = kw.get("model")
+        self.colour = kw.get("colour")
+
+
+my_car = Car(make="Nissan", model="GT-R", colour="Red")
+print(my_car.colour)
