72014ec7e4831f83a43e6876aae8ad4ba13b5272
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2025-01-04 20:51:52 +0100

    made an update

--- day_27/prac.py
+++ day_27/prac.py
@@ -1,8 +1,24 @@
-def add(*args):
-    sum = 0
-    for n in args:
-        sum += n
-    return sum
+# def add(*args):
+#     sum = 0
+#     for n in args:
+#         sum += n
+#     return sum
+#
+#
+# print(add(3, 5, 4, 6))
 
 
-print(add(3, 5, 4, 6))
+def calculate(**kwargs):
+    for key, value in kwargs.items():
+        print(key)
+        print(value)
+
+
+calculate(add=3, multiply=5)
+
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw["make"]
+        self.model = kw["model"]
