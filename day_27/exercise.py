c14794cb8fe223939bb0b605f695600551d3bc32
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2025-01-12 11:56:20 +0100

    made an update

--- day_27/exercise.py
+++ day_27/exercise.py
@@ -14,19 +14,19 @@ window.minsize(width=500, height=300)
 # Label
 my_label = Label(text="Label", font=("Arial", 24, "bold"))
 my_label.config(text="More Text")
-my_label.grid(column=1., row=1)
+my_label.grid(column=0, row=0)
 
 # Buttons
 new_button = Button(text="New Button", command=button_clicked)
-new_button.grid(column=3, row=1)
+new_button.grid(column=2, row=0)
 
 button = Button(text="Button", command=button_clicked)
-button.grid(column=3, row=1)
+button.grid(column=1, row=1)
 
 # Entry
 input = Entry(width=10)
 print(input.get())
-input.grid(column=4, row=3)
+input.grid(column=2, row=3)
 
 
 window.mainloop()
