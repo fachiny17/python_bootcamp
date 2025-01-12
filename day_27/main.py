5d8b74653320501793df22d9aea0063413c2d3e0
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2025-01-12 11:34:21 +0100

    made an update

--- day_27/main.py
+++ day_27/main.py
@@ -1,11 +1,22 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+# Button
+button = Button(text="CLick Me", command=button_clicked)
+button.pack()
 
 window.mainloop()
