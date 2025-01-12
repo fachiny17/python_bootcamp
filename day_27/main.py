0cc48c94cce944b26fd5a171d4c4481e6836067c
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2025-01-12 11:40:53 +0100

    made an update

--- day_27/main.py
+++ day_27/main.py
@@ -13,10 +13,17 @@ window.minsize(width=500, height=300)
 
 # Label
 my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.config(text="More Text")
 my_label.pack()
 
 # Button
 button = Button(text="CLick Me", command=button_clicked)
 button.pack()
+
+# Entry
+input = Entry(width=10)
+print(input.get())
+input.pack(side="left")
+
 
 window.mainloop()
